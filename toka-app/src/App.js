f4a121b48c858b910605e3ea188a866f1e35aa19
RenodoFTW <dominiccarter584@gmail.com>
2023-03-02 15:28:33 +0000

    TAILWIND SUCKS

---
+++ toka-app/src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="container bg-blue h-screen w-100">
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/> 
+      <p className="text-3xl text-gray-700 font-bold mb-5">
+        Welcome!
+      </p>
+      <p className="text-gray-500 text-lg">
+        React and Tailwind CSS in action
+      </p>
+    </div>
+  );
+}
+
+export default App;
